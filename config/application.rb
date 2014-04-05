6298255b0050f7a97bccf5fcf22ae73a87bba71f
bit2 <vishwasjain2010@vishwass-MacBook-Pro.local>
2014-04-05 15:02:58 +0530

    added PG configs

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module VishApp
     #  g.test_framework  :rspec
     # g.orm             :mongoid
     end 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
